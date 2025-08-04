b25ed1905fe78c112c0db1096105842147add61b
haein0617 <ini0617@naver.com>
2025-08-04 21:31:21 +0900

    [Bronze V] Title: 이상한 기호, Time: 212 ms, Memory: 17720 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		System.out.println(math(a,b));
-	}
-	public static int math(int a, int b) {
-		return (a+b)*(a-b);
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong(); // 입력값이 클 수 있으므로 long 사용
+        long b = sc.nextLong();
+        
+        long result = (a + b) * (a - b);
+        System.out.println(result);
+    }
 }
