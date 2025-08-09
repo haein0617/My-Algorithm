82378f1ec7946630e7ebecff49f6937d47d36470
haein0617 <ini0617@naver.com>
2025-08-09 17:12:52 +0900

    [Bronze II] Title: 다이얼, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String a = br.readLine();
+		int time = 0;
+		for (int i = 0; i < a.length(); i++) {
+			switch(a.charAt(i)) {
+			case 'A':
+			case 'B':
+			case 'C':
+				time+=3;
+				break;
+			case 'D':
+			case 'E':
+			case 'F':
+				time+=4;
+				break;
+			case 'G':
+			case 'H':
+			case 'I':
+				time+=5;
+				break;
+			case 'J':
+			case 'K':
+			case 'L':
+				time+=6;
+				break;
+			case 'M':
+			case 'N':
+			case 'O':
+				time+=7;
+				break;
+			case 'P':
+			case 'Q':
+			case 'R':
+			case 'S':
+				time+=8;
+				break;
+			case 'T':
+			case 'U':
+			case 'V':
+				time+=9;
+				break;
+			case 'W':
+			case 'X':
+			case 'Y':
+			case 'Z':
+				time+=10;
+				break;
+			}
+		}
+		System.out.println(time);
+		
+		
+	}
+}
