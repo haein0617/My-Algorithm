1e36bb30e76987a09293db5717f243c87b939069
haein0617 <ini0617@naver.com>
2025-09-10 13:50:50 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 264 ms, Memory: 79,792 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,94 +1,91 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static class Edge{
-		int from, to;
+	static int n, x, y;
+
+	static class Node implements Comparable<Node> {
+		int to;
 		double cost;
-		public Edge(int from, int to, double cost) {
-			this.from = from;
+
+		public Node(int to, double cost) {
 			this.to = to;
 			this.cost = cost;
 		}
+
+		@Override
+		public int compareTo(Node o) {
+			return Double.compare(this.cost, o.cost);
+		}
+
 	}
-	static int[] p;
-	private static int n;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		StringBuilder sb = new StringBuilder();
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= t ; tc++) { // 테케 시작
-			n = Integer.parseInt(br.readLine()); // 섬 개수
-			
+		for (int tc = 1; tc <= t; tc++) {// 테케 시작
+			n = Integer.parseInt(br.readLine());
+
 			int[] r = new int[n];
 			int[] c = new int[n];
-			
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			for (int i = 0; i < n; i++) { // 섬 좌표 r 받기
-				r[i] = Integer.parseInt(st.nextToken());
-			}
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int i = 0; i < n; i++) { // 섬 좌표 c 받기
-				c[i] = Integer.parseInt(st.nextToken());
-			}
-			double E = Double.parseDouble(br.readLine()); // 세율
-			
-			ArrayList<Edge> edgeList = new ArrayList<>();
+
+			StringTokenizer stX = new StringTokenizer(br.readLine(), " ");
+			StringTokenizer stY = new StringTokenizer(br.readLine(), " ");
 			for (int i = 0; i < n; i++) {
-				for (int j = i+1; j < n; j++) {
-					long dR = (long)r[i]-r[j];
-					long dC = (long)c[i]-c[j];
-					double cost = (dR*dR + dC*dC) * E;
-					
-					edgeList.add(new Edge(i,j,cost));
+				r[i] = Integer.parseInt(stX.nextToken());
+				c[i] = Integer.parseInt(stY.nextToken());
+			}
+
+			double E = Double.parseDouble(br.readLine()); // 세율
+
+			boolean[] visited = new boolean[n];
+			PriorityQueue<Node> pq = new PriorityQueue<>();
+
+			double totalCost = 0;
+			int count = 0;
+
+			pq.offer(new Node(0, 0));
+
+			while (!pq.isEmpty()) {
+				Node cur = pq.poll();
+				int curVertex = cur.to;
+				double cost = cur.cost;
+
+				if (visited[curVertex]) {
+					continue;
 				}
+
+				visited[curVertex] = true;
+				totalCost += cost;
+				count++;
+
+				if (count == n)
+					break;
+
+				for (int nextVertex = 0; nextVertex < n; nextVertex++) {
+					// 아직 방문하지 않은 정점이라면
+					if (!visited[nextVertex]) {
+						// 거리의 제곱(L^2) 계산. 오버플로우 방지를 위해 long 사용
+						long dx = (long) r[curVertex] - r[nextVertex];
+						long dy = (long) c[curVertex] - c[nextVertex];
+
+						// 환경 부담금 계산 (E * L^2)
+						double edgeCost = (dx * dx + dy * dy) * E;
+
+						// 계산된 간선 정보를 우선순위 큐에 추가
+						pq.offer(new Node(nextVertex, edgeCost));
+					}
+				}
+
 			}
-			//일단 모든 간선이랑 가격 구했음. 이제 정렬 하면 될 듯?
-			
-			edgeList.sort((o1, o2) -> Double.compare(o1.cost, o2.cost));
-			
-			p = new int[n+1];
-			
-			for (int i = 0; i <= n; i++) {
-				makeSet(i);
-			}
-			
-			
-			double ans = 0;
-			int pick = 0;
-			
-			for (Edge now : edgeList) { // 향상된 for문으로 변경 (더 깔끔함)
-	            int px = findSet(now.from);
-	            int py = findSet(now.to);
-	            if (px != py) {
-	                union(px, py);
-	                ans += now.cost;
-	                pick++;
-	            }
-	            if (pick == n - 1) break; // n-1개 간선을 뽑으면 MST 완성
-	        }
-			
-			long Answer = Math.round(ans);
-			sb.append("#").append(tc).append(" ").append(Answer).append("\n");
-			
-		}// 테케 끝
+			long finalAnswer = Math.round(totalCost);
+            sb.append("#").append(tc).append(" ").append(finalAnswer).append("\n");
+
+		} // 테케 끝
 		System.out.println(sb.toString());
-		
-	}
-	private static void union(int px, int py) {
-		p[py] = px;
-	}
-	private static int findSet(int i) {
-		if(i != p[i]) {
-			p[i] = findSet(p[i]);
-		}
-		return p[i];
-		
-	}
-	private static void makeSet(int i) {
-		p[i]=i;
 	}
 }
