1754ceca3468f51bb7402371a8e681ba25188dbe
haein0617 <ini0617@naver.com>
2025-09-12 15:08:13 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 157 ms, Memory: 33,280 KB -BaekjoonHub

---
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String[] args) throws Exception{
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for (int tc = 1; tc <= 10 ; tc++) { // 테케 시작 (10개)
+			sb.append("#").append(tc).append(" ");
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int v = Integer.parseInt(st.nextToken()); // 정점의 수 (3 <= v <= 1000)
+			int e = Integer.parseInt(st.nextToken()); // 간선의 수 (2 <= e <= 3000)
+			
+			int[][] graph = new int[v+1][v+1]; // 인접행렬
+			int[] inDegree = new int[v+1]; //진입차수 저장 배열 ( 선행 과제 수 )
+			
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int i = 0; i < e; i++) {
+				int in = Integer.parseInt(st.nextToken());
+				int out = Integer.parseInt(st.nextToken());
+				
+				graph[in][out] = 1;
+				inDegree[out]++;
+			}// 입력 완료
+			
+			Queue<Integer> q = new ArrayDeque<>();
+			
+			for(int i = 1; i<v+1; i++) {
+				if(inDegree[i] == 0) {
+					q.offer(i);
+				}
+			}
+			
+			while(!q.isEmpty()) {
+				int cur = q.poll();
+				sb.append(cur).append(" ");
+				
+				for (int to = 1; to < v+1 ; to++) {
+					
+					if(graph[cur][to]==1) {
+						inDegree[to]--;
+						graph[cur][to] = 0;
+						
+						if(inDegree[to] == 0) {
+							q.offer(to);
+						}
+					}
+				}
+			}
+			sb.append("\n");
+		}// 테케 끝
+		System.out.println(sb.toString());
+		
+		
+	}// 메인 끝
+}//클래스 끝
