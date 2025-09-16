4e60ed2e1cb43d5ec4dec9de8c26836db4a7665f
haein0617 <ini0617@naver.com>
2025-09-17 01:30:33 +0900

    [Gold IV] Title: 동전 1, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int n = Integer.parseInt(st.nextToken());//동전 수
+		int k = Integer.parseInt(st.nextToken());// 만들 금액
+		
+		
+		int[] coins = new int[n];
+		for (int i = 0; i < n; i++) {
+			coins[i] = Integer.parseInt(br.readLine());
+		}// 동전 입력 끝
+		
+		int[] dp = new int[k+1]; // i원을 만드는 경우의 수
+		dp[0] = 1;
+		
+		for (int i = 0; i < n; i++) {
+			int coin = coins[i];
+			
+			for (int j = coin; j <= k; j++) {
+				dp[j] += dp[j-coin];
+			}
+		}
+		System.out.println(dp[k]);
+		
+		
+		
+		
+	}
+}
